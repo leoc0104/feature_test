144bfe3fb8685eeaa787885cd8fe97de3cdaf9e0
leoc0104 <leocamargo0104@gmail.com>
2023-05-17 10:11:15 -0300

    Setting ImageController

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ModelImage;
+
+class ImageController extends Controller
+{
+    protected $objImage;
+
+    public function __construct()
+    {
+        $this->objImage = new ModelImage();
+    }
+
+    public function index()
+    {
+        $images = $this->objImage->paginate(5);
+        return view('index', compact('images'));
+    }
+}
