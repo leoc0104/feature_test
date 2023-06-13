e8cebe90c0c038de906c7938fd5d9ededc83c144
Leonardo Camargo <leocamargo0104@gmail.com>
2023-06-13 18:28:41 -0300

    ModelImage

--- app/Models/ModelImage.php
+++ app/Models/ModelImage.php
@@ -9,9 +9,5 @@ class ModelImage extends Model
 {
     protected $table = 'images';
 
-<<<<<<< Updated upstream
-    protected $fillable = ['username', 'action', 'width', 'height'];
-=======
     protected $fillable = ['username', 'action', 'width', 'height', 'image', 'extension'];
->>>>>>> Stashed changes
 }
